c09d22ca8fc1741384358954f4d650dcfc3a8870
Jundev <junhyeong3289@gmail.com>
2020-11-05 17:31:54 +0900

    add socket.io

--- server/index.js
+++ server/index.js
@@ -6,19 +6,43 @@ const cors = require('cors');
 const Router = require('./routes/route');
 const workerRouter = require('./routes/worker')
 const getDataRouter = require('./routes/getdata')
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-app.use(cors())
+
+app.use(cors({ credentials: true}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+//   app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin: http://localhost:3000');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept-Type');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     next();
+// })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+console.log(io)
+app.set('socketio', io)
+io.on("connection", (socket) => {
+    console.log("연결됨")
+    socket.on("disconnect", () => {
+        console.log("연결 끊김1")
+    });
+});
+
 app.use('/routes', Router)
 app.use('/routes', workerRouter)
 app.use('/routes', getDataRouter)
+// app.use('/routes', socketRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+http.listen(3002, () => {
+    console.log(`Example app listening at http://localhost:3002`)
 })
--- server/routes/worker.js
+++ server/routes/worker.js
@@ -3,25 +3,24 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-let interval_getData = null;
+
 router.post("/worker", async (req, res) => {
-    const { minute, count } = req.body;
+    let io = req.app.get('socketio');
+    io.on("connection", (socket) => {
     let reqCount = 0;
+    
+    let interval_getData = null;
+
+    const { minute, count } = req.body;
     const ms = 60000;
+
     const getData = async () => {
         try {
             const gaxlaxyInfo = await db.galaxy.findOne(
                 { order: [['id', 'DESC']] }
             )
-            console.log(reqCount)
-            if (Number(count) - 1 === Number(reqCount)) {
-                console.log(count, reqCount, count === reqCount, "끝 아니냐")
-
-                clearInterval(interval_getData);
-
-            }
-
             const orderedAmount = Math.floor(Math.random() * (gaxlaxyInfo.secondCharge - gaxlaxyInfo.firstCharge + 1) + gaxlaxyInfo.firstCharge);
+
             if (gaxlaxyInfo) {
                 const galaxyReq = await axios.post('https://api.galaxy365.biz/api/v1/trade/create', {
                     optionCode: gaxlaxyInfo.optionCode,
@@ -31,7 +30,7 @@ router.post("/worker", async (req, res) => {
                     callPut: gaxlaxyInfo.callPut
                 })
 
-                await db.galaxytrade.create({
+                const createNewData = await db.galaxytrade.create({
                     optionCode: gaxlaxyInfo.optionCode,
                     expireTime: gaxlaxyInfo.expireTime,
                     orderedAmount: orderedAmount,
@@ -39,7 +38,25 @@ router.post("/worker", async (req, res) => {
                     callPut: gaxlaxyInfo.callPut,
                     result: galaxyReq.status
                 })
+
+                
+                io.emit("newDataToClient", createNewData);
                 reqCount += 1;
+               
+                
+                if (Number(count) === Number(reqCount)) {
+                    console.log("데이터 생성끝")
+                    reqCount = 0;
+                    
+                        socket.emit("disconnect", "끝")
+                        socket.on("disconnect", () => {
+                            console.log("연결 끊김")
+                        });
+                   
+                   
+                    clearInterval(interval_getData);
+                }
+            // return res.json({ data: createNewData})
             }
         } catch (error) {
             console.log(error);
@@ -53,7 +70,7 @@ router.post("/worker", async (req, res) => {
     } catch (error) {
         console.log(err);
     }
-
+});
 })
 
 
