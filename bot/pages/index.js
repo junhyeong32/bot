6a70a00462014a9054519cb322950957bef0aa6f
Jundev <junhyeong3289@gmail.com>
2020-11-09 13:42:24 +0900

    complete

--- bot/pages/index.js
+++ bot/pages/index.js
@@ -1,84 +1,48 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Button, Input, Table, Select } from 'antd';
 import { useCallback, useState, useEffect } from 'react';
 import axios from 'axios';
-import moment from 'moment'
 import { boardTableColumns, dataSource } from '../components/tabledata'
+import io from 'socket.io-client'
 
-//데이터 리랜더링 라이브로 띄어주기
 
-//입력 받는 값 제대로 받기(expireTime)
-//
+const socket = io("http://localhost:3002");
 export default function Home() {
   const [minute, setMinute] = useState("")
   const [count, setCount] = useState("")
-  const [runCount, setRunCount] = useState(0)
   const [firstCharge, setfirstCharge] = useState("")
   const [secondCharge, setSecondCharge] = useState("")
-  const [optionCode, setOptionCode] = useState("")
-  // const [expireTime, setExpireTime] = useState("")
-  const [orderedAmount, setOrderedAmount] = useState("")
-  const [currency, setCurrency] = useState("")
   const [callPut, setCallPut] = useState("")
   const [result, setResult] = useState("")
   const [dataList, setDatadList] = useState([])
 
   const [loading, setLoading] = useState(false)
 
-  let term = 1; 
+  let term = 1;
   let now = new Date().getTime();
   let expireTime = now - (now % 60000) + (60000 * term);
-  
+
   const sendData = useCallback(async () => {
-    // if(!minute || !count || !firstCharge || !secondCharge || callPut){
-    //   return alert("값 입력 요망")
-    // }
-    return axios.post('http://localhost:3001/routes/route', {
+    setLoading(true)
+    return axios.post('http://localhost:3001/routes/request', {
       minute: minute,
       count: count,
       firstCharge: firstCharge,
       secondCharge: secondCharge,
-      optionCode: "Bitcoin", 
-      expireTime: 10, 
-      currency: 840, 
-      callPut: Number(callPut), 
+      expireTime: expireTime,
+      callPut: callPut,
     })
       .then((res) => {
-        setLoading(true)
-        worker()
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
       })
   })
 
-  const worker = async () => {
-    return axios.post('http://localhost:3001/routes/worker', {
-      minute: minute,
-      count: count,
-      firstCharge: firstCharge,
-      secondCharge: secondCharge,
-      optionCode: "Bitcoin", 
-      expireTime: 10, 
-      orderedAmount: 10, 
-      currency: 840, 
-      callPut: Number(callPut), 
-    })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }
-
   const getData = async () => {
     return axios.post('http://localhost:3001/routes/getdata')
       .then((res) => {
-        console.log(";;")
-
-        setRunCount(res.data.data[0].id)
         setDatadList(res.data.data)
       })
       .catch((err) => {
@@ -86,10 +50,14 @@ export default function Home() {
       })
   }
 
-  
   useEffect(() => {
+    socket.on('newData', (data) => {
+      setResult(data)
+      getData()
+    });
+
     getData()
-  }, [runCount])
+  }, [])
 
   const { Option } = Select;
   function handleChange(value) {
@@ -97,28 +65,25 @@ export default function Home() {
   }
   return (
     <div className={styles.mainContainer}>
-      <div>
-        <Input type="text" className={styles.input} value={minute} onChange={(e => { setMinute(e.target.value) })}></Input>분 마다
+      <div className={styles.inputMainContainer}>
+        <div className={styles.inputContainer}>
+          <div>
+            <Input type="text" className={styles.input} value={minute} onChange={(e => { setMinute(e.target.value) })}></Input>분 마다
+          </div>
+          <div>
+            <Input type="text" className={styles.input} value={count} onChange={(e => { setCount(e.target.value) })} ></Input> 회를
+          </div>
+          <div>
+            <Input type="number" className={styles.input} value={firstCharge} onChange={(e => { setfirstCharge(e.target.value) })}></Input> ~
+            <Input type="number" className={styles.input} value={secondCharge} onChange={(e => { setSecondCharge(e.target.value) })}></Input> 금액으로<br />
+          </div>
+          <Select className={styles.input} defaultValue="Call" value={callPut} onChange={handleChange}>
+              <Option value={"1"}>Call</Option>
+              <Option value={-"1"}>Put</Option>
+            </Select>
+        </div>
+        <Button onClick={sendData} loading={loading} className={styles.input} >요청</Button>
       </div>
-      <div>
-        <Input type="text" className={styles.input} value={count} onChange={(e => { setCount(e.target.value) })} ></Input> 회를
-      </div>
-      <div>
-        <Input type="number" className={styles.input} value={firstCharge} onChange={(e => { setfirstCharge(e.target.value) })}></Input> ~
-      <Input type="number" className={styles.input} value={secondCharge} onChange={(e => { setSecondCharge(e.target.value) })}></Input> 금액으로<br />
-        <Select className={styles.input} defaultValue="Call" value={callPut} onChange={handleChange}>
-          <Option value={"1"}>Call</Option>
-          <Option value={-"1"}>Put</Option>
-        </Select>
-      </div>
-      <Button onClick={sendData} >요청</Button>
-      {/* loading={loading} */}
-
-      {/* <div>
-        <Table>
-          
-        </Table>
-      </div> */}
 
       <div>
         <Table columns={boardTableColumns} dataSource={dataSource(dataList)} pagination={{ position: ['bottomCenter'], pageSize: 10 }} scroll={{ y: 550 }} />
@@ -127,3 +92,5 @@ export default function Home() {
 
   )
 }
+
+
